b3ecb01d22467b17371744644a130972a63f22e6
Vismaya T Surendran <vismayatsurendran@gmail.com>
2018-06-21 22:58:41 +0530

    Updated with mapbox key

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoidmlzbWF5YS1zdXJlbmRyYW4iLCJhIjoiY2ppbTA2cnNjMGxkNDNrdW5mOGNkYmxjYiJ9.MZ_dX3sM5snL3dCwKAlV4w',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
