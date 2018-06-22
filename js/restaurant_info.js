a0adc7afc15266ddc2b2beeb4e4a46f13512cdee
Vismaya T Surendran <vismayatsurendran@gmail.com>
2018-06-23 00:29:57 +0530

    Image attribute set

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,6 +89,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute('alt', restaurant.name);
+  image.setAttribute('alt', `An image of ${restaurant.name}`);
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
